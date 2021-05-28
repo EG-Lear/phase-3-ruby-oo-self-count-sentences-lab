cb0b9aa2aa6109032e99c38942a0bbecc7f78a15
Garret <gwiats@gmail.com>
2021-05-27 20:01:02 -0400

    commit tree 131176a8a573074ff7b11e0b4e68444f25928f37

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    # binding.pry
+    countable = []
+    split_string = self.split(/[.!?]/)
+    split_string.each do |string|
+      if string != ''
+        countable << string
+      end
+    end
+    countable.length
   end
 end
